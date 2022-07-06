bfbdfde35a80b101fcb66e2ba1926467d0fdcec1
burakdinceeer@gmail.com <burakdinceeer@gmail.com>
2022-07-06 20:27:22 +0300

    Add project files.

---
+++ RestaurantList.Case/Entities/Cafe.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantList.Case.Entities
+{
+    public class Cafe
+    {
+        public int CafeId { get; set; }
+        public string CafeName { get; set; }
+        public string CuisineType { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public int Capacity { get; set; }
+        public decimal Rating { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ RestaurantList.Case/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantList.Case.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImgUrl { get; set; }
+        public virtual ICollection<Cafe> cafes { get; set; }
+    }
+}
