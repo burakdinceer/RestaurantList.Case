45c6e8c019f3e0f5493879c340f1431bcd33e4ec
burakdinceeer@gmail.com <burakdinceeer@gmail.com>
2022-07-06 21:00:42 +0300

    GetList and GetId

---
+++ RestaurantList.Case/Controllers/CafeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantList.Case.Entities;
+using RestaurantList.Case.Entities.Dto;
+using RestaurantList.Case.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestaurantList.Case.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CafeController : ControllerBase
+    {
+        private readonly IGenericRepository<Cafe> _cafeRepository;
+        private readonly IGenericRepository<Category> _categoryRepository;
+
+        public CafeController(IGenericRepository<Cafe> cafeRepository, IGenericRepository<Category> categoryRepository)
+        {
+            _cafeRepository = cafeRepository;
+            _categoryRepository = categoryRepository;
+            
+        }
+
+        [HttpGet]
+        [Route("GetListCafe")]
+        public List<CafeResponseDto> GetListCafe()
+        {
+            var list = _cafeRepository.GetAll().Select(x=> new CafeResponseDto
+            {
+                CafeId = x.CafeId,
+                CafeName = x.CafeName,
+                CuisineType = x.CuisineType,
+                Location = x.Location,
+            }).ToList();
+            return list;
+        }
+
+        [HttpGet]
+        [Route("GetSingleCafe")]
+        public CafeResponseDto GetSingleCafe(int id)
+        {
+            var result = _cafeRepository.GetAll().Where(x => x.CafeId == id).Select(x => new CafeResponseDto
+            {
+                CafeId = x.CafeId,
+                CafeName = x.CafeName,
+                CuisineType = x.CuisineType,
+                Location = x.Location,
+            }).SingleOrDefault();
+            return result;
+        }
+
+
+    }
+}
---
+++ RestaurantList.Case/Entities/Dto/CafeResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantList.Case.Entities.Dto
+{
+    public class CafeResponseDto
+    {
+        public int CafeId { get; set; }
+        public string CafeName { get; set; }
+        public string CuisineType { get; set; }
+        public string Location { get; set; }
+    }
+}
